7451e43a2976af99425f247bae880d383232f40c
Mike Lumetta <mjlumetta@gmail.com>
2013-08-21 21:10:54 -0500

    Generate Sessions controller and handle signin failure

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # sign in and redirect to profile
+    else
+      flash.now[:error] = 'Invalid email/password combination' # not quite right
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
